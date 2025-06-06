eff33fdc408079209f54eb761818bb133b6bb37a
Andrew Tolmachev <parsoph85@gmail.com>
2025-06-06 22:18:03 +0300

    Update TaskTwo.jsx

--- homework/homework/src/task-two/TaskTwo.jsx
+++ homework/homework/src/task-two/TaskTwo.jsx
@@ -3,7 +3,8 @@ import RenderCounter from './render-counter/RenderCounter';
 import './TaskTwo.css';
 
 export default function TaskTwo() {
-    const update = useUpdate()
+    const update = useUpdate();
+
     return (
         <div className="TaskTwo">
             <button onClick={update}>Обновить компонент</button>
@@ -13,30 +14,41 @@ export default function TaskTwo() {
     )
 }
 
-const Root = () => {
-    const [value, setValue] = React.useState('')
-    const handleChange = (event) => {
-        setValue(event.target.value)
-    }
+// Мемоизация компонента Root
+const Root = React.memo(() => {
+    const [value, setValue] = React.useState('');
+
+    // useCallback для handleChange, чтобы не пересоздавать функцию при каждом рендере
+    const handleChange = React.useCallback((event) => {
+        setValue(event.target.value);
+    }, []);
+
     return (
         <form className="form-container">
             Введенное значение: {value}
             {/*<RenderCounter />*/}
             <Input onChange={handleChange} />
         </form>
-    )
-}
+    );
+});
 
-const Input = ({ onChange }) => {
+// Мемоизация компонента Input
+const Input = React.memo(({ onChange }) => {
     return (
         <div className="input-container">
-            <input type="text" className="input-field" name="value" onChange={onChange} />
+            <input
+                type="text"
+                className="input-field"
+                name="value"
+                onChange={onChange}
+            />
             {/*<RenderCounter />*/}
         </div>
-    )
-}
+    );
+});
 
+// Хук для обновления всего компонента TaskTwo
 function useUpdate() {
-    const [, setCount] = React.useState(0)
-    return () => { setCount(counter => counter + 1) }
+    const [, setCount] = React.useState(0);
+    return () => { setCount(c => c + 1); }
 }
